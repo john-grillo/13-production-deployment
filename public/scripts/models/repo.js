8ec7407663fd011a084fd99d36488674f1d73955
john-grillo <john.grillo@pcc.edu>
2017-06-14 13:59:34 -0700

    rewrote ajax call to use get method

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,20 +7,23 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
+    // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
+    $.get('github/user/repos')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
-
-  repos.with = attr => repos.all.filter(repo => repo[attr]);
+  //deprecated: no longer needed since we aren't using an .ajax call.
+  //repos.with = attr => repos.all.filter(repo => repo[attr]);
 
   module.repos = repos;
 })(app);
+
+//replacement of .get with .ajax:
+// ajax({
+//       url: `https://api.github.com/user/repos`,
+//       type: 'GET',
+//       headers: {'Authorization': `token ${githubToken}`}
+//     })
